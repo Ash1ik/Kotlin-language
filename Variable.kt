69d18d26326ca33e4bae6a2544481fc02f28e2be
Ashik Iqbal <109750205+Ash1ik@users.noreply.github.com>
2023-10-04 22:09:59 +0600

    Add files via upload

---
+++ Variable.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // We can reassign var type
+    var score = 5
+    score = 10 // here you see that we can reassign score
+
+    // We cannot reassign val type
+    val message = "Hello World"
+    //message = "Ashik Iqbal" // here you see that we cannot reassign message
+
+    println(message)
+    println(score)
+
+    //If you run this code (Kotlin: Val cannot be reassigned) this message will show in the output
+}
---
+++ datatype.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    // variable can be initiate like (var: data types)
+
+    var score: Int = 10
+    var temperature: Double = 90.0
+    var israinning: Boolean = false
+    var alphabet: Char = 'A'
+    var message: String = "Ashik Iqbal"
+
+    println("This is Integer: "+score)
+    println("This is Double: "+temperature)
+    println("This is Boolean: "+israinning)
+    println("This is Char: "+alphabet)
+    println("This is String: "+message)
+
+    /*
+    output:
+        10
+        90.0
+        false
+        A
+        Ashik Iqbal
+     */
+}
